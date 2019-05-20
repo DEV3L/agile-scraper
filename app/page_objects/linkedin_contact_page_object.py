bc21e247f0d989abd13f9a8564052a5a2c8dd390
DEV3L <jus.beall@gmail.com>
2019-05-20 17:17:04 -0400

    Add write buffer

--- app/page_objects/linkedin_contact_page_object.py
+++ app/page_objects/linkedin_contact_page_object.py
@@ -9,14 +9,14 @@ class LinkedInContactPageObject(PageObject):
     def scrape_contact(self):
         self.browser.get(self.link)
 
-        self.wait_by_id("profile-content")
+        self.wait_by_class_name("pv-top-card-v2-section__link--contact-info")
 
         dist_value = self.browser.find_element_by_class_name('dist-value')
 
         if dist_value.text != '1st':
             return
 
-        contact_info_link = self.browser.find_elements_by_xpath("//*[@data-control-name='contact_see_more']")[0]
+        contact_info_link = self.browser.find_element_by_xpath("//*[@data-control-name='contact_see_more']")
         contact_info_link.click()
 
         phone = ''
--- linkedin-contact-scraper.py
+++ linkedin-contact-scraper.py
@@ -14,31 +14,21 @@ Env().read_env(path="./.env")
 user_login_value = os.environ['USER_LOGIN']
 user_password_value = os.environ['USER_PASS']
 
-data_file = './data/gene_connections.csv'
+write_records_buffer = 10
+
+file_prefix = user_login_value.split('@')[0]
+data_file = f'./data/{file_prefix}_connections.csv'
+results_file = f'./output/{file_prefix}_results.csv'
 
 
-class Contact:
-    def __init__(self, row_data):
-        self.first_name = row_data[0]
-        self.last_name = row_data[1]
-        self.email = row_data[2]
-        self.phone = None
-        self.company = row_data[3]
-        self.position = row_data[4]
-        self.connected_on = row_data[5]
+def run():
+    print(f'Begin LinkedIn Contact Scraper - For {user_login_value}')
+    print("- for the modern Jew on the go, now at MedaSync")
 
-    def dump(self):
-        return [self.first_name, self.last_name, self.email, self.phone, self.company, self.position, self.connected_on]
+    contact_results = read_results_file(results_file)
+    mapped_contacts = {contact.key: contact for contact in contact_results}
 
-
-with open(data_file) as csvfile:
-    csv_data = csv.reader(csvfile)
-    data_rows = [row for row in csv_data][1:]
-    contacts = [Contact(row) for row in data_rows]
-
-if __name__ == '__main__':
-    print("Begin LinkedIn Contact Scraper - For the modern Jew at MedaSync")
-
+    contacts = read_data_file(data_file)
     print(f'Starting to Process {len(contacts)} contacts')
 
     browser = webdriver.Chrome()
@@ -49,44 +39,87 @@ if __name__ == '__main__':
     print(f'Logged into LinkedIn with user {user_login_value}')
 
     contact_count = 0
-
     for contact in contacts:
-        print(f'Looking for contact: {contact.first_name} {contact.last_name}')
+        mapped_contact = mapped_contacts.get(contact.key)
 
-        linkedin_feed_page = LinkedInFeedPageObject(browser)
-        if not linkedin_feed_page.search_contact(contact):
+        if mapped_contact and (mapped_contact.email or mapped_contact.phone):
             continue
 
-        linkedin_search_results = LinkedInSearchResultsPageObject(browser)
-        contact_links = linkedin_search_results.scrape_contact_links()
-
-        for link in contact_links:
-            linkedin_contact_page = LinkedInContactPageObject(browser, link)
-            contact_details = linkedin_contact_page.scrape_contact()
-
-            if not contact_details:
-                continue
-
-            contact.email = contact_details['email'].replace('\n', '').replace('Email', '')
-            contact.phone = contact_details['phone'].replace('\n', '').replace('Phone', '')
-
-            contact_count += 1
-            break
-
-        if contact_count % 10 == 0:
-            print(f'Writing records: {contact_count} of {len(contacts)}')
-
-            with open('results.csv', mode='w') as results:
-                results_writer = csv.writer(results, delimiter=',')
-
-                results_writer.writerow(
-                    ['First Name', 'Last Name', 'Email', 'Phone', 'Company', 'Title', 'Connected On'])
-                for contact in contacts:
-                    results_writer.writerow(contact.dump())
+        print(f'Looking for contact: {contact.first_name} {contact.last_name}')
+        contact_count = scrape_contact(contact, contact_count, contacts, browser)
 
     browser.close()
+    write_records(contacts, contact_count)
 
-    with open('results.csv', mode='w') as results:
+    print(f'Finished LinkedIn Contact Scraper - For {user_login_value}')
+
+
+def filter_contacts(contacts):
+    results = read_results_file(result_file)
+    results = [result for result in results if result.email or result.phone]
+    filtered_contacts = []
+    for contact in contacts:
+        is_found = False
+
+        for result_contact in results:
+            if result_contact.key == contact.key:
+                is_found = True
+                break
+
+        if not is_found:
+            filtered_contacts.append(contact)
+
+    return filtered_contacts
+
+
+def read_data_file(data_file: str):
+    data_rows = _read_file(data_file)
+    return [Contact.from_row_data(row) for row in data_rows]
+
+
+def read_results_file(results_file: str):
+    try:
+        results = _read_file(results_file)
+        return [Contact.from_result_data(result) for result in results]
+    except Exception as e:
+        return []
+
+
+def _read_file(file_name: str):
+    with open(file_name) as csvfile:
+        csv_data = csv.reader(csvfile)
+        data_rows = [row for row in csv_data][1:]
+    return data_rows
+
+
+def scrape_contact(contact, contact_count: int, contacts, browser):
+    linkedin_feed_page = LinkedInFeedPageObject(browser)
+    if not linkedin_feed_page.search_contact(contact):
+        return contact_count
+
+    linkedin_search_results = LinkedInSearchResultsPageObject(browser)
+    contact_links = linkedin_search_results.scrape_contact_links()
+
+    for link in contact_links:
+        linkedin_contact_page = LinkedInContactPageObject(browser, link)
+        contact_details = linkedin_contact_page.scrape_contact()
+
+        if not contact_details:
+            continue
+
+        contact.email = contact_details['email'].replace('\n', '').replace('Email', '')
+        contact.phone = contact_details['phone'].replace('\n', '').replace('Phone', '')
+
+        if should_write(contact_count):
+            write_records(contacts, contact_count)
+
+        return contact_count + 1
+
+    return contact_count
+
+
+def write_records(contacts, contact_count):
+    with open(results_file, mode='w') as results:
         print(f'Writing records: {contact_count} of {len(contacts)}')
 
         results_writer = csv.writer(results, delimiter=',')
@@ -94,3 +127,57 @@ if __name__ == '__main__':
         results_writer.writerow(['First Name', 'Last Name', 'Email', 'Phone', 'Company', 'Title', 'Connected On'])
         for contact in contacts:
             results_writer.writerow(contact.dump())
+
+
+def should_write(contact_count):
+    return contact_count and contact_count % write_records_buffer == 0
+
+
+class Contact:
+    def __init__(self):
+        self.first_name = None
+        self.last_name = None
+        self.email = None
+        self.phone = None
+        self.company = None
+        self.position = None
+        self.connected_on = None
+
+    @property
+    def key(self):
+        return self.first_name + self.last_name + self.position
+
+    def dump(self):
+        return [self.first_name, self.last_name, self.email, self.phone, self.company, self.position, self.connected_on]
+
+    @staticmethod
+    def from_row_data(row_data):
+        contact = Contact()
+
+        contact.first_name = row_data[0]
+        contact.last_name = row_data[1]
+        contact.email = row_data[2]
+        contact.phone = None
+        contact.company = row_data[3]
+        contact.position = row_data[4]
+        contact.connected_on = row_data[5]
+
+        return contact
+
+    @staticmethod
+    def from_result_data(result_data):
+        contact = Contact()
+
+        contact.first_name = result_data[0]
+        contact.last_name = result_data[1]
+        contact.email = result_data[2]
+        contact.phone = result_data[3]
+        contact.company = result_data[4]
+        contact.position = result_data[5]
+        contact.connected_on = result_data[6]
+
+        return contact
+
+
+if __name__ == '__main__':
+    run()
